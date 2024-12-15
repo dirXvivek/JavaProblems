016abbe8743c254c58a1be8edca11d76674749c6
vivek <yadulinux@gmail.com>
2024-12-15 16:32:24 +0530

    implementation in code

--- ClassEmployee.java
+++ ClassEmployee.java
@@ -1,2 +1,19 @@
-public class ClassEmployee {
+class EmpClass {
+
+    int salary;
+    String name;
+    char secName;
+
+    public int getEmpSalary(){
+        return salary;
+    }
+
+    public String getName(){
+        String usrName;
+        return usrName = "My name is "+ name;
+    }
+
+    public void setName(char n){
+        secName = n;
+    }
 }
