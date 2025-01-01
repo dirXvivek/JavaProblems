3e15dcde81b99ccd488fe8ce99e22eb813c9471b
vivek <yadulinux@gmail.com>
2025-01-01 21:06:48 +0530

    modified function

--- ChallengeLibrary.java
+++ ChallengeLibrary.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class Library{
 String [] books= new String[50];
     public void addBook(String n){
-      books[0]=n;
+
     }
     public void issueBook(){
 
@@ -22,13 +22,14 @@ public class ChallengeLibrary {
     //class instance
         Library lb = new Library();
         while(true){
-            System.out.println("1: Add Book\n 2: Issue Book \n 3: Return Book \n Available Book");
+            System.out.println("1: Add Book\n 2: Issue Book \n 3: Return Book \n 4: Available Book\n");
             int userSelect = input.nextInt();
             switch (userSelect){
                 case 1:
-                    System.out.print("Add the book here!: ");
+                    System.out.println("Add the book here!: ");
                     String bookName = input.nextLine();
                     lb.addBook(bookName);
+                    break;
             }
         }
     }
